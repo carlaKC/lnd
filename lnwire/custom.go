f83fb527101b33c1ee5d634ffa6c729416514767
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2022-07-06 09:26:46 +0200

    lnwire: allow overriding of protocol messages outside of custom range
    Add the ability to specify messages < CustomRangeStart that will still
    be treated like custom messages by lnd (rather than unknown ones). This
    allows code external to lnd to handle protocol messages that are *not
    yet known to lnd*.

--- lnwire/custom.go
+++ lnwire/custom.go
@@ -2,13 +2,54 @@ package lnwire
 
 import (
 	"bytes"
-	"errors"
+	"fmt"
 	"io"
 )
 
-// CustomTypeStart is the start of the custom type range for peer messages as
-// defined in BOLT 01.
-var CustomTypeStart MessageType = 32768
+var (
+	// CustomTypeStart is the start of the custom type range for peer
+	// messages as defined in BOLT 01.
+	CustomTypeStart MessageType = 32768
+
+	// CustomTypeOverride contains a set of message types < CustomTypeStart
+	// that lnd allows to be treated as custom messages. This allows us to
+	// override messages reserved for the protocol level and treat them as
+	// custom messages.
+	CustomTypeOverride []MessageType
+)
+
+// SetCustomOverrides validates that the set of override types are outside of
+// the custom message range (there's no reason to override messages that are
+// already within the range), and updates the CustomTypeOverride global to hold
+// this set of message types.
+func SetCustomOverrides(overrideTypes []uint64) error {
+	CustomTypeOverride = make([]MessageType, len(overrideTypes))
+
+	for i, t := range overrideTypes {
+		msgType := MessageType(t)
+
+		if msgType >= CustomTypeStart {
+			return fmt.Errorf("can't override type: %v, already "+
+				"in custom range", t)
+		}
+
+		CustomTypeOverride[i] = msgType
+	}
+
+	return nil
+}
+
+// IsCustomOverride returns a bool indicating whether the message type is one
+// of the protocol messages that we override for custom use.
+func IsCustomOverride(t MessageType) bool {
+	for _, override := range CustomTypeOverride {
+		if t == override {
+			return true
+		}
+	}
+
+	return false
+}
 
 // Custom represents an application-defined wire message.
 type Custom struct {
@@ -20,10 +61,12 @@ type Custom struct {
 // interface.
 var _ Message = (*Custom)(nil)
 
-// NewCustom instanties a new custom message.
+// NewCustom instantiates a new custom message.
 func NewCustom(msgType MessageType, data []byte) (*Custom, error) {
-	if msgType < CustomTypeStart {
-		return nil, errors.New("msg type not in custom range")
+	if msgType < CustomTypeStart && !IsCustomOverride(msgType) {
+		return nil, fmt.Errorf("msg type: %v not in custom range: %v "+
+			"and not overridden: %v", msgType, CustomTypeStart,
+			CustomTypeOverride)
 	}
 
 	return &Custom{
