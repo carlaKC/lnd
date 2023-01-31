47402be2ff6733ca5541372d4e1e4a512167a56d
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-01-31 16:22:48 -0500

    multi: add representation of blinded paths
    This commit introduces the concept of a blinded payment, which
    consists of a blinded path, aggregate parameters and minimal
    constraints for the path and basic/sane validation.
    
    Payment relay info and payment constraints are placed in the
    record package, because when the time comes to add support for
    forwarding of blinded payments, we'll extract these structs
    as TLVs in our encrypted onion blob. For now, this is not
    important (it's just an easy way to represent this data),
    but Aggregate(Relay/Constraints) aliases are used in the
    routing package to emphasize that these a blinded payment uses
    aggregate versions of the per-hop struct.

---
+++ routing/blinding_test.go
@@ -0,0 +1,75 @@
+package routing
+
+import (
+	"errors"
+	"testing"
+
+	sphinx "github.com/lightningnetwork/lightning-onion"
+	"github.com/stretchr/testify/require"
+)
+
+// TestBlindedPathValidation tests validation of blinded paths.
+func TestBlindedPathValidation(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		payment *BlindedPayment
+		err     error
+	}{
+		{
+			name:    "no path",
+			payment: &BlindedPayment{},
+			err:     ErrNoBlindedPath,
+		},
+		{
+			name: "no relay info",
+			payment: &BlindedPayment{
+				BlindedPath: &sphinx.BlindedPath{},
+			},
+			err: ErrNoRelayInfo,
+		},
+		{
+			name: "no constraints",
+			payment: &BlindedPayment{
+				BlindedPath: &sphinx.BlindedPath{},
+				RelayInfo:   &AggregateRelay{},
+			},
+			err: ErrNoConstraints,
+		},
+		{
+			name: "insufficient hops",
+			payment: &BlindedPayment{
+				BlindedPath: &sphinx.BlindedPath{
+					BlindedHops: []*sphinx.BlindedHopInfo{},
+				},
+				RelayInfo:   &AggregateRelay{},
+				Constraints: &AggregateConstraints{},
+			},
+			err: ErrInsufficientBlindedHops,
+		},
+		{
+			name: "valid",
+			payment: &BlindedPayment{
+				BlindedPath: &sphinx.BlindedPath{
+					BlindedHops: []*sphinx.BlindedHopInfo{
+						{},
+					},
+				},
+				RelayInfo:   &AggregateRelay{},
+				Constraints: &AggregateConstraints{},
+			},
+		},
+	}
+
+	for _, testCase := range tests {
+		testCase := testCase
+
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := testCase.payment.Validate()
+			require.True(t, errors.Is(err, testCase.err))
+		})
+	}
+}
