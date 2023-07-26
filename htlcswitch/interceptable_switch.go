d2b90cb3108eea5424394bfc75bd8b2b5eb0876a
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-07-26 13:59:58 -0400

    multi: add custom records to interceptor rpc

--- htlcswitch/interceptable_switch.go
+++ htlcswitch/interceptable_switch.go
@@ -128,6 +128,10 @@ type FwdResolution struct {
 	// FailureCode is the failure code that is to be passed back to the
 	// sender if action is FwdActionFail.
 	FailureCode lnwire.FailCode
+
+	// UpdateAddTLVs is an optional set of custom-range TLVs to include in
+	// the outgoing update_add_htlc, only used for FwdActionResume.
+	UpdateAddTLVs record.CustomSet
 }
 
 type fwdResolution struct {
@@ -373,7 +377,7 @@ func (s *InterceptableSwitch) resolve(res *FwdResolution) error {
 
 	switch res.Action {
 	case FwdActionResume:
-		return intercepted.Resume(nil)
+		return intercepted.Resume(res.UpdateAddTLVs)
 
 	case FwdActionSettle:
 		return intercepted.Settle(res.Preimage)
