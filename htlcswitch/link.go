f1b9a85548681c02f92bc84032aa3526b71da854
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-04-02 09:50:13 -0400

    htlcswitch: add NextBlinding to ForwardingInfo and set in UpdateAddHtlc
    When we have a HTLC that is part of a blinded route, we need to include
    the next ephemeral blinding point in UpdateAddHtlc for the next hop. The
    way that we handle the addition of this key is the same for introduction
    nodes and relaying nodes within the route.

--- htlcswitch/link.go
+++ htlcswitch/link.go
@@ -3345,9 +3345,10 @@ func (l *channelLink) processRemoteAdds(fwdPkg *channeldb.FwdPkg,
 				// Otherwise, it was already processed, we can
 				// can collect it and continue.
 				addMsg := &lnwire.UpdateAddHTLC{
-					Expiry:      fwdInfo.OutgoingCTLV,
-					Amount:      fwdInfo.AmountToForward,
-					PaymentHash: pd.RHash,
+					Expiry:        fwdInfo.OutgoingCTLV,
+					Amount:        fwdInfo.AmountToForward,
+					PaymentHash:   pd.RHash,
+					BlindingPoint: fwdInfo.NextBlinding,
 				}
 
 				// Finally, we'll encode the onion packet for
@@ -3390,9 +3391,10 @@ func (l *channelLink) processRemoteAdds(fwdPkg *channeldb.FwdPkg,
 			// create the outgoing HTLC using the parameters as
 			// specified in the forwarding info.
 			addMsg := &lnwire.UpdateAddHTLC{
-				Expiry:      fwdInfo.OutgoingCTLV,
-				Amount:      fwdInfo.AmountToForward,
-				PaymentHash: pd.RHash,
+				Expiry:        fwdInfo.OutgoingCTLV,
+				Amount:        fwdInfo.AmountToForward,
+				PaymentHash:   pd.RHash,
+				BlindingPoint: fwdInfo.NextBlinding,
 			}
 
 			// Finally, we'll encode the onion packet for the
