6e17e75dfbbc3f5ae46c778ef6e5aa76774ac1ee
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-05-22 09:04:23 -0400

    lnrpc: set a zero value endorsement signal on sender outgoing htlc
    Before we have sufficient signaling in the network to relay this
    signal, set a zero value experimental endorsement value on the sender's
    outgoing htlc. Once the network is relaying this signal and a flag day
    has been set, we'll be able to set a non-zero value here.

--- rpcserver.go
+++ rpcserver.go
@@ -44,6 +44,7 @@ import (
 	"github.com/lightningnetwork/lnd/channeldb/graphsession"
 	"github.com/lightningnetwork/lnd/channeldb/models"
 	"github.com/lightningnetwork/lnd/channelnotifier"
+	"github.com/lightningnetwork/lnd/clock"
 	"github.com/lightningnetwork/lnd/contractcourt"
 	"github.com/lightningnetwork/lnd/discovery"
 	"github.com/lightningnetwork/lnd/feature"
@@ -744,6 +745,11 @@ func (r *rpcServer) addDeps(s *server, macService *macaroons.Service,
 		},
 		SetChannelAuto:     s.chanStatusMgr.RequestAuto,
 		UseStatusInitiated: subServerCgs.RouterRPC.UseStatusInitiated,
+		ShouldSetExperimentalEndorsement: func() bool {
+			return clock.NewDefaultClock().Now().Before(
+				EndorsementExperimentEnd,
+			)
+		},
 	}
 
 	genInvoiceFeatures := func() *lnwire.FeatureVector {
